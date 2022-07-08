b331a41662835015efe3705890ea52c4a30c209d
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-08 15:17:12 +0100

    Update databasestart.php

--- php/databasestart.php
+++ php/databasestart.php
@@ -1,8 +1,8 @@
 
 <?php
-$servername = "localhost:3360";
-$database = "pixelpri_database_pixelprints";
-$username = "pixelpri_User_pixelprints";
+$servername = "localhost";
+$database = "database_pixelprints";
+$username = "User_pixelprints";
 $password = "XD#oc.]%Z$9R";
 
 
