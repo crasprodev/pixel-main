6164b5e89e6de6a11a79a24f36f751ea595b7933
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-08 14:50:35 +0100

    Update databasestart.php

--- php/databasestart.php
+++ php/databasestart.php
@@ -6,6 +6,6 @@ $db_name="database_pixelprints";
 $conexao=mysqli_connect($servername,$username,$pass,$db_name );
 if (!$conexao) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
-
+exit;
 }
 ?>
