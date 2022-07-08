a56b1215ec6380b5e22f57bf01ceb7fe2666a941
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-08 17:13:24 +0100

    Update sendmailexample.php

--- php/sendmailexample.php
+++ php/sendmailexample.php
@@ -1,36 +1,12 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-require 'PHPMailer-master/src/Exception.php';
-require 'PHPMailer-master/src/PHPMailer.php';
-require 'PHPMailer-master/src/SMTP.php';
 function send_email($emailrecipiente,$titulo, $conteudo)
 {
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->Mailer = "smtp";
-$mail->SMTPAuth   = TRUE;
-$mail->SMTPSecure = "tls"; 
-$mail->Port       = 587;
-$mail->Host       = "smtp.gmail.com";
-$mail->Username   = "botqniqnwfwfiwenf@gmail.com";
-$mail->SMTPDebug  = 2; 
-  // 5Y4Lyqn9pQf&$-?m
-$mail->Password   = "jkrmhhhfbcorkieg";
-$mail->IsHTML(true);
-$mail->SetLanguage("pt", 'class/phpMailer/language/');
-$mail->AddAddress($emailrecipiente, "recipient-name");
-$mail->SetFrom("botj6883@gmail.com", "no-replybot");
-$mail->AddReplyTo($emailrecipiente, "no-reply");
-$mail->AddCC($emailrecipiente, "cc-recipient-name");
-$mail->Subject = $titulo;
-$content = $conteudo;
-$mail->MsgHTML($content); 
-if(!$mail->Send()) {
-  return;
-  var_dump($mail);
-} else {
-  return;
-}}
+ $from ="botaccount@pixelprints.pt";
+$Subject = $titulo;
+  $titulo = $titulo;
+  $conteudo = $conteudo;
+  $header = "from no-reply-bot";
+mail($from,$Subject,$conteudo ,$header)
+}
 ?>
