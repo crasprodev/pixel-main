5b3616a8f197ca21a023f03be0adbd28b865529e
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-08 15:48:42 +0100

    Update databasestart.php

--- php/databasestart.php
+++ php/databasestart.php
@@ -11,7 +11,10 @@ $conexao = mysqli_connect($servername, $username, $password, $database);
 // Connection Check
 if(!$conexao)
 {
-echo 'Ligação ao servidor impossivel';exit;
+echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
 else{
    echo "Connected Successfully!";
