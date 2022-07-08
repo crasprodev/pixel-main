b4974c93135f9c9855da9aaea46d26914f9c618f
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-08 15:09:28 +0100

    Update databasestart.php

--- php/databasestart.php
+++ php/databasestart.php
@@ -9,7 +9,10 @@ $password = "[hwhKjQh)X{0";
 // Create connection using musqli_connect function
 $conexao = mysqli_connect($servername, $username, $password, $database);
 // Connection Check
-
+if(!$conexao)
+{
+echo 'Ligação ao servidor impossivel';exit;
+}
 else{
    echo "Connected Successfully!";
    $conn->close();
