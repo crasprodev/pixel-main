85a30aeedcb6f0892bae4bca14d0a1feb59f7186
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-08 14:46:15 +0100

    Update databasestart.php

--- php/databasestart.php
+++ php/databasestart.php
@@ -4,8 +4,9 @@ $username = "pixelpri_a_user_manager";
 $pass = "Profanus2022!?";
 $db_name="database_pixelprints";
 $conexao=mysqli_connect($servername,$username,$pass,$db_name );
-if(!$conexao)
-{
-echo 'Ligação ao servidor impossivel';exit;
-}
+if (!$conexao) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 ?>
