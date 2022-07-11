2f5e30a2029d38d62fc404da28f52508491ea7f0
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-11 17:09:47 +0100

    Update nav_login.php

--- php/nav_login.php
+++ php/nav_login.php
@@ -1,6 +1,7 @@
 <?php include("createsession_cookies.php");
 $email = $_POST["email"];
 $password = $_POST["password"];
+echo $password;
 $sessao = $_POST["sessao"];
 echo login($email, $password, $sessao);
 ?>
