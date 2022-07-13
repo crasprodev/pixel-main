9f0723bc57cb2704f6f6ac92e38660644891e9dd
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-13 14:12:52 +0100

    Update User_progress.php

--- php/User_progress.php
+++ php/User_progress.php
@@ -1,1 +1,37 @@
 <?php include ("navbar/navbar.php"); ?>
+
+<body>
+    <style>
+    .progressmodule {
+        margin-top: 12rem;
+        background: red;
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+        text-align: center;
+    }
+
+    .titulo {
+        font-size: 2rem;
+    }
+
+    .sm-titulo {
+        font-size: 1.5rem;
+    }
+    </style>
+    <div class="progressmodule">
+        <div class="titulo">Progresso do seu produto</div><br>
+        <div class="sm-titulo">Estampagem de ol</div>
+        <div class="progress_bars">
+            <div class="progresses">
+
+            </div>
+            <div class="progress_bar"></div>
+        </div>
+    </div>
+</body>
+<script src="../Jquery/jquery.js"></script>
+<script src="../Jquery/loginsignin.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+AOS.init();
+</script>
