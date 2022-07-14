e31d0a972cfee2655c00a45bd69607c2317f92e0
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-14 17:03:14 +0100

    Update Adminpage.php

--- php/Adminpage.php
+++ php/Adminpage.php
@@ -3,7 +3,7 @@
         <div class="Titulo_obra">Folhas de obras: historico de mudanças e status</div>
         <div class="parts_obra">
             <div class="subtitulo_obra" style="margin-bottom: none;">Folhas de obras</div>
-            <div class="userdata" style="display: flex;">
+            <div class="userdata">
                 <div class="searchbox">
                     <select class="category input_appeareance">
 
@@ -44,7 +44,6 @@
                 </div>
             </div>
         </div>
-
     </div>
 </body>
 
