99f1aa38f4ef1ff2917b338cea8fa6d6d80fd188
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-15 14:53:17 +0100

    Update admin pg

--- php/Adminpage.php
+++ php/Adminpage.php
@@ -1,55 +1,96 @@
-<body>
-    <div class="folha_de_obra">
-        <div class="Titulo_obra">Folhas de obras: historico de mudanças e status</div>
-        <div class="parts_obra">
-            <div class="subtitulo_obra" style="margin-bottom: none;">Folhas de obras</div>
-            <div class="userdata" style="display: flex;">
-                <div class="searchbox">
-                    <select class="category input_appeareance">
+<div class="folha_de_obra">
+    <div class="Titulo_obra">Folhas de obras: historico de mudanças e status</div>
+    <div class="parts_obra">
+        <div class="subtitulo_obra" style="margin-bottom: none;">Folhas de obras</div>
+        <div class="userdata" style="display: flex;">
+            <div class="searchbox ">
+                <select class="category input_appeareance">
 
-                    </select>
-                    <input type="text" class="input_appeareance input_pld" name="search" placeholder="Pesquisa aqui">
+                    <option value="codfolha">codfolha</option>
+                    <option value="Informação do utilizador">Informação do utilizador</option>
+                    <option style="display:none"></option>
+                    <option value="descricão">descricão</option>
+                    <option value="localização de ficheiros">localização de ficheiros</option>
+                    <option value="Budget montagem">Budget montagem</option>
+                    <option value="Custo montagem">Custo montagem</option>
+                    <option value="Pessoa_Montagem">Pessoa_Montagem</option>
+                </select>
+                <input type="text" class="input_appeareance input_pld" name="search" placeholder="Pesquisa aqui">
+            </div>
+            <div class="table-data">
+                <table style="border-collapse:collapse;">
+                    <thead>
+                        <tr>
+                            <th>Codigo de folha</th>
+                            <th>Tipo de utilizador associado</th>
+                            <th>Código de utilizador</th>
+                            <th>Descrição</th>
+                            <th>Budget montagem</th>
+                            <th>Custo montagem</th>
+                            <th>Pessoa_Montagem</th>
+                            <th>localização de ficheiros</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="userdata_bg" style="gap: 1rem;">
+            <div class="subtitulo_obra">N.º folhas de obra</div>
+            <div class="label-row"></div>
+            <div class="subtitulo_obra">Dados dos clientes</div>
+            <div class="flex_userdata_group">
+                <div class="userdata_group"><label for="">Codigo de utilizador</label> <input type="text" name="nome"
+                        style="    text-align: center;
+    background: red;
+    cursor: pointer;" readonly=""></div>
+            </div>
+            <div class="flex_userdata_group">
+                <div class="userdata_group"><label for="">Tipo de data</label> <input type="text" name="Codigo"
+                        readonly="" style="text-align: center;"></div>
+                <div class="userdata_group"> <label for="">Nome</label> <input type="text" name="nome" readonly=""
+                        style="text-align: center;">
                 </div>
-                <div class="flex" style='width:100%;gap:0;'>
-                    <div class="table-data">
-                        <table style="border-collapse:collapse;">
-                            <thead>
-                                <tr>
-                                    <th>Codigo de folha</th>
-                                    <th>Tipo de utilizador associado</th>
-                                    <th>Código de utilizador</th>
-                                    <th>Descrição</th>
-                                    <th>Budget montagem</th>
-                                    <th>Custo montagem</th>
-                                    <th>Pessoa_Montagem</th>
-                                    <th>localização de ficheiros</th>
-                                </tr>
-                            </thead>
-                            <tbody></tbody>
-                        </table>
-                    </div>
-                    <div class="newwaytabselection" style="gap:0;  margin-bottom: none;">
-                        <div class="tab table_titulo">Historico de modificações</div>
-                        <div class="tab">Nome categoria horas</div>
-                        <div class="tab">Nome categoria horas</div>
-                        <div class="tab">Nome categoria horas</div>
-                        <div class="tab">Nome categoria horas</div>
-                        <div class="tab">Nome categoria horas</div>
-                        <div class="tab">Nome categoria horas</div>
-                        <div class="tab">Nome categoria horas</div>
-                        <div class="tab">Nome categoria horas</div>
-                        <div class="tab">Nome categoria horas</div>
-                        <div class="tab">Nome categoria horas</div>
-                    </div>
+
+            </div>
+            <div class="flex_userdata_group">
+                <div class="userdata_group"> <label for="">Email</label> <input type="text" name="Email" readonly=""
+                        style="text-align: center;">
                 </div>
+                <div class="userdata_group"> <label for="">NIF</label> <input type="text" name="Contacto" readonly=""
+                        style="text-align: center;"></div>
+            </div>
+            <div class="flex_userdata_group">
+                <div class="userdata_group"> <label for="">Contactos</label> <input type="text" name="Email" readonly=""
+                        style="text-align: center;">
+                </div>
+                <div class="userdata_group"> <label for="">Pessoa-contacto</label> <input type="text" name="Contacto"
+                        readonly="" style="text-align: center;"></div>
             </div>
         </div>
 
+
+        <!-- 
+        Edições e valores da folha de obra
+        -->
+        <div class="version">
+            <div class="descript">
+                <div class="descript_tab">Leandro Bernardo</div>
+                <div class="descript_tab"> designer </div>
+                <div class="descript_tab"> 19:00</div>
+            </div>
+            <div class="descript">
+                <div class="descript_tab">Leandro Bernardo</div>
+                <div class="descript_tab"> designer </div>
+                <div class="descript_tab"> 19:00</div>
+            </div>
+            <div class="descript">
+                <div class="descript_tab">Leandro Bernardo</div>
+                <div class="descript_tab">designer </div>
+                <div class="descript_tab">19:00</div>
+            </div>
+        </div>
     </div>
-</body>
 
-</html>
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.0/jquery.mask.js"></script>
-
-</html>
+</div>
