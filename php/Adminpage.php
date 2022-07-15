c5865e588a23d8a5c884ae1d49c9a8784a4c949d
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-15 18:06:35 +0100

    Update Adminpage.php

--- php/Adminpage.php
+++ php/Adminpage.php
@@ -132,4 +132,69 @@
         <textarea name="" cols="10" class="txtobra" placeholder="Escrever aqui" rows="10" readonly=""
             style="color: rgba(255, 255, 255, 0.68);"></textarea>
     </div>
+    <div class="Titulo_obra">Editar dados e mudar privilégios de utilizadores</div>
+    <div class="userdata" style="display: flex;">
+        <div class="searchbox ">
+            <select class="category input_appeareance">
+
+                <option value="codfolha">codfolha</option>
+                <option value="Informação do utilizador">Informação do utilizador</option>
+                <option style="display:none"></option>
+                <option value="descricão">descricão</option>
+                <option value="localização de ficheiros">localização de ficheiros</option>
+                <option value="Budget montagem">Budget montagem</option>
+                <option value="Custo montagem">Custo montagem</option>
+                <option value="Pessoa_Montagem">Pessoa_Montagem</option>
+            </select>
+            <input type="text" class="input_appeareance input_pld" name="search" placeholder="Pesquisa aqui">
+        </div>
+        <div class="table-data">
+            <table style="border-collapse:collapse;">
+                <thead>
+                    <tr>
+                        <th>Codigo de folha</th>
+                        <th>Tipo de utilizador associado</th>
+                        <th>Código de utilizador</th>
+                        <th>Descrição</th>
+                        <th>Budget montagem</th>
+                        <th>Custo montagem</th>
+                        <th>Pessoa_Montagem</th>
+                        <th>localização de ficheiros</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="userdata_bg" style="gap: 1rem;">
+
+        <div class="flex_userdata_group">
+            <div class="userdata_group"><label for="">Codigo de utilizador</label> <input type="text" name="nome"
+                    readonly="" style="text-align: center;"></div>
+            <div class="userdata_group"> <label for="">Nome</label> <input type="text" name="nome" readonly=""
+                    style="text-align: center;">
+            </div>
+
+        </div>
+        <div class="flex_userdata_group">
+            <div class="userdata_group"> <label for="">Email</label> <input type="text" name="Email" readonly=""
+                    style="text-align: center;">
+            </div>
+            <div class="userdata_group"> <label for="">NIF</label> <input type="text" name="Contacto" readonly=""
+                    style="text-align: center;"></div>
+        </div>
+        <div class="flex_userdata_group">
+            <div class="userdata_group"> <label for="">Contactos</label> <input type="text" name="Email" readonly=""
+                    style="text-align: center;">
+            </div>
+            <div class="userdata_group"> <label for="">Pessoa-contacto</label> <input type="text" name="Contacto"
+                    readonly="" style="text-align: center;"></div>
+        </div>
+        <div class="flex" style="gap: 1rem; display: flex;">
+            <div class="editbtn salvar" style="display:none">Salvar </div>
+            <div class="editbtn edit">Editar <span></span><span></span></div>
+            <div class="editbtn remover" style="background:red;">Remover</div>
+        </div>
+    </div>
 </div>
