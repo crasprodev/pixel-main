f14c34eda55e6d7b754d5c6bdb4e88580235e2f5
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-18 14:28:43 +0100

    Update Adminpage.php

--- php/Adminpage.php
+++ php/Adminpage.php
@@ -215,7 +215,18 @@ let Admin = function(nivel) {
         });
         return array;
     }
-    this.usertype(type) {
+    this.userinfo = function(value) {
+        value === "Informação do utilizador" && $(".searchbox.user").length === 0 ?
+            $(".searchbox")
+            .after(
+                "<div class='searchbox user'><select onchange='method.folhas()'class='category input_appeareance'></select><div><div style='text-align:center'>Introduzido por:</div><div style=' display: flex;align-items: center;justify-content: center;gap: 1rem;'>Ambos<input type='radio' name='same' onchange='method.folhas();'checked='checked' value='0'>administrador<input type='radio' onchange='method.folhas()' name='same' value='1'>Utilizador<input type='radio' name='same' onchange='method.folhas()' value='2'></div></div>"
+            )
+            .siblings(".searchbox.user")
+            .children(".category.input_appeareance")
+            .append(end) :
+            $(".searchbox.user").remove();
+    }
+    this.usertype = function(type) {
         array = [];
         if (type == 1) {
 
