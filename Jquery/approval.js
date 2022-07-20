a8389b9445a473f98597b357dbb4a459964dd761
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-20 12:41:14 +0100

    Update approval.js

--- Jquery/approval.js
+++ Jquery/approval.js
@@ -7,7 +7,7 @@ let aprrov = function (nivel) {
     html[5].shift();
     html[5].shift();
     html[5].splice(4, 3);
-    html.splice(2, 1);
+    html.splice(3, 1);
     console.log(html[5]);
     // html.splice(3, 2, html[5], html[6], html[7], html[4]);
     //
