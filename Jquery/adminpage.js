8425f024f78e433f82a6de92cc2a9dc7905f7a3d
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-20 15:51:24 +0100

    Update adminpage.js

--- Jquery/adminpage.js
+++ Jquery/adminpage.js
@@ -87,6 +87,7 @@ let Admin = function (nivel) {
             let edicao = [[]];
             $(html).each(function (index) {
               $("tbody").append("<tr></tr>");
+              console.log(index);
               for (var i = 6; i <= this.length - 1; i++) {
                 edicao[index].push(this[i]);
                 console.log(edicao);
