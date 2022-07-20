bc798ff2c69ad2b5fff151d8488b0c9e0fd39f2f
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-20 10:38:58 +0100

    Update getusersobra.php

--- php/folhadeobra/getusersobra.php
+++ php/folhadeobra/getusersobra.php
@@ -326,7 +326,7 @@ if($query1)
 }
 
 }
-return json_encode($Resultado);
+return json_encode($description);
 array_splice( $Resultado[$i], 4, 0,...$description[$i] );
 }
     return json_encode($Resultado);
