e6373e543f92e5560489ac0988f0cbadf2f00a43
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-20 14:36:03 +0100

    Update adminpage.js

--- Jquery/adminpage.js
+++ Jquery/adminpage.js
@@ -7,6 +7,17 @@ let Admin = function (nivel) {
     });
     return array;
   };
+  this.definitions = function (html) {
+    html[2] === null ? html.splice(2, 1) : html.splice(3, 1);
+    html.length = 6;
+
+    html[5].shift();
+    html[5].shift();
+    html[5].splice(5, 3);
+    html.splice(3, 2, ...html[5]);
+    html.pop();
+    html.splice(4, 4, html[5], html[6], html[7], html[4]);
+  };
   this.userinfo = function (value) {
     value === "Informação do utilizador" &&
     $(".searchbox.user:eq(0)").length === 0
@@ -76,6 +87,7 @@ let Admin = function (nivel) {
             $("tbody:eq(0)").html("");
             $(html).each(function (index) {
               $("tbody").append("<tr></tr>");
+              admin.definitions(this);
               this.map(function (obj, index) {
                 $("tbody>tr:last-child").append(
                   "<td class='border-right-bottom-top'>" + obj + "</td>"
