6ab165d95bc6e04570778d54465b10b904603e6c
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-20 11:21:22 +0100

    Update approval.js

--- Jquery/approval.js
+++ Jquery/approval.js
@@ -1,6 +1,12 @@
 let aprrov = function (nivel) {
   const approv = this;
-  this.definitions = function (html) {};
+  this.definitions = function (html) {
+    html[2] === null ? html.splice(2, 1) : html.splice(3, 1);
+    //html.pop();
+    //html.pop();
+
+    //html.splice(4, 4, html[5], html[6], html[7], html[4]);
+  };
   this.indexclickfunction = function (index) {
     let selectedindex = index;
     const link = `codfolha=${selectedindex[0]}`;
