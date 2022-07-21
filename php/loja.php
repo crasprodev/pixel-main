7d023d7c905d0ac597d0bbf513aceac1da70733b
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-21 09:09:32 +0100

    change shop

--- php/loja.php
+++ php/loja.php
@@ -1,29 +1,11 @@
 <?php include ("navbar/navbar.php"); ?>
 
 <body>
-    <div class="solos">
-        <div class="titulo-s">Progresso do produto</div>
-        <div class="titulo-s">Nome do produto</div>
-        <input type="range" max="100" min="0" disabled>
-    </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.0/jquery.mask.js">
-    </script>
 
-    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.js"></script>
-    <script src="../Jquery/loginsignin.js"></script>
 </body>
 
 </html>
 <div>
-    <?php
-
-if (!empty($_POST))
-{
-
-}
-
-?>
 
     <script src="../Jquery/jquery.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
