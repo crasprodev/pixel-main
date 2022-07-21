ac5971a014d3e5a68e5dfbf204e7b76c508b798f
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-21 16:02:09 +0100

    Update jquery.js

--- Jquery/jquery.js
+++ Jquery/jquery.js
@@ -127,7 +127,7 @@ $(".links:eq(2)").click(function () {
   location.href = "../php/#servicos";
 });
 $(".links:eq(3)").click(function () {
-  location.href = "../php/loja";
+  location.href = "../php/loja.php";
 });
 $(".links:eq(4)").click(function () {
   location.href = "../php/#revenda";
