844b61bbf49bf60327d1b509be6a6b0a10b2d9a1
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-21 10:55:01 +0100

    Update loja.php

--- php/loja.php
+++ php/loja.php
@@ -18,7 +18,7 @@
 }
 
 .shoptitle {
-    font-size: 1.7rem;
+    font-size: 1.7rem + 1vw;
     position: absolute;
     bottom: 30%;
     left: 10%;
@@ -26,7 +26,7 @@
 
 .txtbig {
 
-    font-size: 3rem;
+    font-size: 3rem + 1vw;
 }
 </style>
 
