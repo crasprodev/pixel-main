9247c5badfbd028973eb2c5ebbd9be321bbc80da
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-25 15:53:47 +0100

    Update loja.php

--- php/loja.php
+++ php/loja.php
@@ -179,7 +179,7 @@
     $(".textselect").on("click", function() {
         const textselect = $(this).prop("aes-prop");
         var link = "categoria = ".textselect;
-        inserturl(link);
+        inserturl(textselect);
     });
     </script>
 </div>
