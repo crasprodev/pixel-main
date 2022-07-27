3132411f6a17fbc4e74939da5c81746d201cdcb7
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-27 10:43:51 +0100

    Update adminpage.js

--- Jquery/adminpage.js
+++ Jquery/adminpage.js
@@ -19,19 +19,26 @@ let Admin = function (nivel) {
     newhtml.splice(4, 4, newhtml[5], newhtml[6], newhtml[7], newhtml[4]);
   };
   this.userinfo = function (value) {
-    value === "Informação do utilizador" &&
-    $(".searchbox.user:eq(0)").length === 0
-      ? $(".searchbox:eq(0)")
-          .after(
-            "<div class='searchbox user'><select onchange='admin.usertype(0)'class='category input_appeareance'></select><div><div style='text-align:center'>Introduzido por:</div><div style=' display: flex;align-items: center;justify-content: center;gap: 1rem;'>Ambos<input type='radio' name='same' onchange='admin.folhas();'checked='checked' value='0'>administrador<input type='radio' onchange='admin_this.folhas()' name='same' value='1'>Utilizador<input type='radio' name='same' onchange='admin.usertype(0);' value='2'></div></div>"
-          )
-          .siblings(".searchbox.user:eq(0)")
-          .children(".category.input_appeareance")
-          .append(...admin_this.option(options_user))
-          .siblings("div input")
-          .eq(0)
-          .prop("checked", true)
-      : $(".searchbox.user").remove();
+    if (
+      value === "Informação do utilizador" &&
+      $(".searchbox.user:eq(0)").length === 0
+    ) {
+      $(".searchbox:eq(0)")
+        .after(
+          "<div class='searchbox user'><select class='category input_appeareance'></select><div><div style='text-align:center'>Introduzido por:</div><div style=' display: flex;align-items: center;justify-content: center;gap: 1rem;'>Ambos<input type='radio' name='same' onchange='admin.folhas();'checked='checked' value='0'>administrador<input type='radio' onchange='admin_this.folhas()' name='same' value='1'>Utilizador<input type='radio' name='same' value='2'></div></div>"
+        )
+        .siblings(".searchbox.user:eq(0)")
+        .children(".category.input_appeareance")
+        .append(...admin_this.option(options_user))
+        .siblings("div input")
+        .eq(0)
+        .prop("checked", true);
+      $(".userdata:eq(0) .searchbox.user").on("change", function () {
+        admin.usertype(0);
+      });
+      return;
+    }
+    $(".searchbox.user").remove();
   };
   const options_user = [
     "Cod_id",
