3e61fb6759f66b756f24fd4c108e6d0f534c671c
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-27 15:31:25 +0100

    Update adminpage.js

--- Jquery/adminpage.js
+++ Jquery/adminpage.js
@@ -79,7 +79,7 @@ let Admin = function (nivel) {
         $.ajax({
           type: "POST",
           url: "folhadeobra/do_folhadetrabalho.php",
-          //dataType: "json",
+          dataType: "json",
           data: {
             request: "getfolhanivel",
             text: $(".input_appeareance:eq(1)").val(),
