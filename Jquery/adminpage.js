ced07f1fddbc6574b088f000db8608b765478fdb
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-07-28 17:41:54 +0100

    Update adminpage.js

--- Jquery/adminpage.js
+++ Jquery/adminpage.js
@@ -27,7 +27,7 @@ let Admin = function (nivel) {
         `<div class="descript"> <div class="descript_tab">${obj[8]}</div> <div class="descript_tab">${obj[7]} </div> <div class="descript_tab"> ${obj[9]}</div> </div>`
       );
       $(".version:last-child").click(() => {
-        $(".txtobra").eq(0).val(descriptionfilter[2]);
+        $(".txtobra").eq(0).val(obj[2]);
       });
     });
     const link = `codfolha=${selectedindex[0]}`;
