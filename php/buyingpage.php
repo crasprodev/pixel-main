2e98999bf0793de272c3efa03a24d4a80de8cf6d
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-09-06 14:49:02 +0100

    oi

--- php/buyingpage.php
+++ php/buyingpage.php
@@ -13,5 +13,7 @@
         <div class="title_text">Pack Portátil LENOVO IdeaPad 3 15ITL05 (15.6'' - Intel Core i5-1135G7 - RAM: 12 GB - 512
             GB SSD - Intel Iris Xe Graphics) + Mochila + Rato</div>
         <div class="preco">59€</div>
+        <div class="description"></div>
+        <div class="comment"></div>
     </div>
 </body>
