9b713c3aee6b9da9ec848a6e8ce621de82610c4f
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-09-07 10:29:10 +0100

    Update buyingpage.php

--- php/buyingpage.php
+++ php/buyingpage.php
@@ -39,3 +39,13 @@
             forma ideal. Inclui Jogo de 22 Peças sortidas (brocas e Bits).</div>
     </div>
 </body>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+<script>
+AOS.init();
+</script>
+<script src="../Jquery/jquery.js"></script>
+
+</html>
+<script>
+</script>
