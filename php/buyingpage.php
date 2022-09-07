f1b348edf9a6e5b8d90fca790054c9482f95f959
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-09-07 13:57:35 +0100

    rechange

--- php/buyingpage.php
+++ php/buyingpage.php
@@ -3,40 +3,9 @@
 <body>
     <div class="category"></div>
     <div class="mn_principal">
-
-        <div class="imagens_compra">
-            <div class="imagem_cmp">
-            </div>
-            <div class="multiple_img">
-                <div class="multiplefreeimg">
-                    <div class="img_sm_mult"></div>
-                </div>
-            </div>
-        </div>
-
-        <div class="title_text">Pack Portátil LENOVO IdeaPad 3 15ITL05 (15.6'' - Intel Core i5-1135G7 - RAM: 12 GB -
-            512
-            GB SSD - Intel Iris Xe Graphics) + Mochila + Rato</div>
-        <div class="loja_comprar">
-            <div class="loja_money">59€</div>
-            <div class="adicionarcart">Adicionar ao cart</div>
-        </div>
-        <div class="description">Berbequim/ Aparafusadora a bateria TC-CD 18-2 Li (1x1,5Ah) Caixa de 2 velocidades para
-            um aparafusamento potente e uma perfuração rápida 18 + 1 regulações de binário Bloqueio automático de fuso
-            para troca simples de ferramentas Design fino com pega suave ergonómica Mandril sem chave de manga única
-            Incl. Luz LED para iluminar a área de trabalho Inclui Jogo de 34 Bits sortidas Acumulador 18 V | 1500 mAh |
-            LiIo Tempo de carregamento 3-5h Número de velocidades 2 Rotações em vazio (velocidade 1) 0-350 min^-1
-            Rotações em vazio (velocidade 2) 0-1250 min^-1 Binário máx. junta dura 38 Nm Número de Níveis de Ajuste 19
-            settings Bucha 10 mm | manga única O berbequim aparafusador sem fios TC-CD 18-2 Li da Einhell é uma
-            ferramenta versátil para aparafusamento potente e perfuração rápida. Equipado com uma engrenagem de duas
-            velocidades, é a ajuda perfeita em casa, na oficina e na garagem. Independentemente de o berbequim
-            aparafusador sem fios TC-CD 18-2 Li da Einhell servir para perfurar madeira, pedra ou metal, ou de a
-            ferramenta ser usada para soltar e fixar parafusos: graças à forma estreita com áreas Softgrip ergonómicas,
-            o berbequim aparafusador adapta-se bem à mão e tem uma operação agradável. O berbequim aparafusador sem fios
-            TC-CD 18-2 Li da Einhell está equipado com um bloqueio de fuso automático para uma troca simples da
-            ferramenta e um mandril de aperto rápido de uma manga com dez milímetros. O berbequim aparafusador sem fios
-            acaba com o flagelo da má iluminação do local de utilização: a luz LED clara ilumina a área de trabalho de
-            forma ideal. Inclui Jogo de 22 Peças sortidas (brocas e Bits).</div>
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
 </body>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
