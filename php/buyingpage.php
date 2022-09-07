6f0d6766e04d9dc3add54e81b6a8e434513dcfde
leandromicael123 <107862577+leandromicael123@users.noreply.github.com>
2022-09-07 09:48:43 +0100

    Update buyingpage.php

--- php/buyingpage.php
+++ php/buyingpage.php
@@ -19,6 +19,7 @@
         </div>
         <div class="loja_comprar">
             <div class="loja_money">59€</div>
+            <div class="adicionarcart">Adicionar ao cart</div>
         </div>
         <div class="description">Berbequim/ Aparafusadora a bateria TC-CD 18-2 Li (1x1,5Ah) Caixa de 2 velocidades para
             um aparafusamento potente e uma perfuração rápida 18 + 1 regulações de binário Bloqueio automático de fuso
